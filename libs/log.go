cea017378ff0087052f3d2a2bbfb059859e4e712
oktapascal <oktaiscool@gmail.com>
2024-07-25 16:49:12 +0700

    Add documentation comment

--- libs/log.go
+++ libs/log.go
@@ -11,6 +11,20 @@ import (
 var loggerConsole = logrus.New()
 var loggerFile = logrus.New()
 
+// CreateLoggerConsole creates a logrus.Entry instance for logging to the console.
+// It accepts an optional http.Request parameter, which is used to enrich the log output with request-specific information.
+// If the request parameter is nil, the function logs a generic timestamp.
+//
+// The function initializes a logrus logger with the following settings:
+// - Output: os.Stdout
+// - Formatter: logrus.TextFormatter with ForceColors set to true
+// - Level: logrus.TraceLevel
+//
+// The function returns a logrus.Entry instance with the following fields:
+// - at: The current timestamp in the format "2024-07-25 04:05:10"
+// - method: The HTTP method of the request (if the request parameter is not nil)
+// - uri: The request URI (if the request parameter is not nil)
+// - ip: The remote address of the request (if the request parameter is not nil)
 func CreateLoggerConsole(r *http.Request) *logrus.Entry {
 	loggerConsole.SetOutput(os.Stdout)
 	loggerConsole.SetFormatter(&logrus.TextFormatter{ForceColors: true})
@@ -30,6 +44,16 @@ func CreateLoggerConsole(r *http.Request) *logrus.Entry {
 	})
 }
 
+// createLogsDir creates the logs directory if it does not exist.
+//
+// The function checks if the "logs" directory exists in the current working directory.
+// If the directory does not exist, it attempts to create it with the specified permissions (0755).
+//
+// Parameters:
+// None
+//
+// Return:
+// error: An error if the directory creation fails, or nil if the directory already exists or was successfully created.
 func createLogsDir() error {
 	logsDir := "logs"
 	if _, err := os.Stat(logsDir); os.IsNotExist(err) {
@@ -42,6 +66,20 @@ func getLogFileName() string {
 	return filepath.Join("logs", time.Now().Format("2006-01-02")+".log")
 }
 
+// CreateLoggerFile initializes and configures a logrus Logger instance for logging to a file.
+// The function creates a logs directory if it does not exist, and then opens a log file with the current date as the filename.
+// The Logger instance is configured with the following settings:
+// - Formatter: logrus.JSONFormatter
+// - Level: logrus.WarnLevel
+// - Output: The opened log file
+//
+// If any errors occur during the directory creation or file opening process, the function logs a fatal error using the CreateLoggerConsole function.
+//
+// Parameters:
+// None
+//
+// Return:
+// *logrus.Logger: A configured logrus Logger instance for logging to a file
 func CreateLoggerFile() *logrus.Logger {
 	err := createLogsDir()
 	if err != nil {
